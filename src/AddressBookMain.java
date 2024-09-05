59f52d7ec9c98a6f9cc34d4517d5552410f102ed
Saichandu090 <marrisaichandu143@gmail.com>
2024-09-05 17:17:54 +0530

    Added ability to add a new contact to AddressBook from console

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,11 +1,42 @@
+import java.util.*;
+
 public class AddressBookMain
 {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Address Book Program!!");
+        List<AddressBook> contact=new ArrayList<AddressBook>();
 
-        AddressBook contact=new AddressBook("M","Sai Chandu","Gandhinagar","Kadapa","AP",516004,6301941367l,"marrisaichandu143@gmail.com");
+        Scanner sc=new Scanner(System.in);
 
-        System.out.println(contact);
+        char userChoice='Y';
+        while (userChoice == 'Y') {
+            AddressBook a=new AddressBook();
+            System.out.println("Enter contact details to add into AddressBook");
+
+            System.out.println("First_Name : ");
+            a.setFirstName(sc.next());
+            System.out.println("Last_Name");
+            a.setLastName(sc.next());
+            System.out.println("Address :");
+            a.setAddress(sc.next());
+            System.out.println("City :");
+            a.setCity(sc.next());
+            System.out.println("State :");
+            a.setState(sc.next());
+            System.out.println("ZipCode :");
+            a.setZip(sc.nextInt());
+            System.out.println("PhoneNumber :");
+            a.setPhoneNumber(sc.nextLong());
+            System.out.println("Email :");
+            a.setEmail(sc.next());
+
+            contact.add(a);
+            System.out.println("Entered Contacts are :");
+            System.out.println(contact);
+
+            System.out.println("Do you wish to add any contact details : if yes press 'Y' or else 'N' :");
+            userChoice = sc.next().charAt(0);
+        }
+
     }
 }
